bd5b2a9656d860239146a891f68821364ea636d5
akcorp2003 <kuanga2016@ucla.edu>
2015-11-25 00:18:15 -0800

    Added Java project files
    Look into src folder for the files.

---
+++ src/main_entry.java
@@ -0,0 +1,12 @@
+
+public class main_entry {
+	public static void main(String args[])
+	{
+		String filename = args[0]; //is it the first arg?
+		
+		//I'm assuming we need a CSV parser as well?
+		
+		//I'm also thinking we need some sort of node class to
+		//hold all the branches and decisions
+	}
+}
