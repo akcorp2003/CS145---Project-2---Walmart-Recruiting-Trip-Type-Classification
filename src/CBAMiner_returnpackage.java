145c8de84b58aadb5702eda32b73b1bcfb083124
akcorp2003 <kuanga2016@ucla.edu>
2015-12-01 12:14:19 -0800

    Added column index mapping integer to string
    Added additional data structure that can map integer to string.

--- src/CBAMiner_returnpackage.java
+++ src/CBAMiner_returnpackage.java
@@ -4,10 +4,20 @@ import java.util.Map;
 public class CBAMiner_returnpackage {
 	
 	private Map<String, ArrayList<Integer>> _CBAMatrix;
-	private Map<String, Integer> _CBAMatrix_index;
+	private Map<String, Integer> _CBAMatrix_INDEX_nametoindex;
+	private Map<Integer, String> _CBAMatrix_INDEX_indextoname;
 	
-	public CBAMiner_returnpackage(Map<String, ArrayList<Integer>> CBAMatrix, Map<String, Integer> CBAMatrix_index){
+	public CBAMiner_returnpackage(Map<String, ArrayList<Integer>> CBAMatrix, Map<String, Integer> CBAMatrix_index_s_i, Map<Integer, String> CBAMatrix_index_i_s){
 		_CBAMatrix = CBAMatrix;
-		_CBAMatrix_index = CBAMatrix_index;
+		_CBAMatrix_INDEX_nametoindex = CBAMatrix_index_s_i;
+		_CBAMatrix_INDEX_indextoname = CBAMatrix_index_i_s;
+	}
+	
+	public Map<String, Integer> get_matrixindex_nametoindex(){
+		return _CBAMatrix_INDEX_nametoindex;
+	}
+	
+	public Map<Integer, String> get_matrixINDEX_indextoname(){
+		return _CBAMatrix_INDEX_indextoname;
 	}
 }
